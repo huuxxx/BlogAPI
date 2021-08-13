3ba22112d86ecaba0e0a0e8b904dc9c74af74a1c
Huxley Dowling <hdowling87@hotmail.com>
2021-08-13 16:41:48 +1000

    Pre processor for Startup

--- BlogAPI/Startup.cs
+++ BlogAPI/Startup.cs
@@ -79,8 +79,15 @@ namespace BlogAPI
                 app.UseDeveloperExceptionPage();
             }
 
+#if DEBUG
             app.UseCors(
                 options => options.WithOrigins("http://localhost:3000", "http://localhost:53150").AllowAnyMethod().AllowAnyHeader()
+#endif
+#if RELEASE
+            app.UseCors(
+	            options => options.WithOrigins("https://www.hux-dev.com", "https://blog-cms-weld.vercel.app").AllowAnyMethod().AllowAnyHeader()
+            );
+#endif
             );
 
             app.UseDefaultFiles();
