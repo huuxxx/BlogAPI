fa2a483dc0021b8528a2b25c76e858b595822568
Huxley Dowling <hdowling87@hotmail.com>
2021-08-13 16:48:04 +1000

    Fix typo

--- BlogAPI/Startup.cs
+++ BlogAPI/Startup.cs
@@ -82,13 +82,14 @@ namespace BlogAPI
 #if DEBUG
             app.UseCors(
                 options => options.WithOrigins("http://localhost:3000", "http://localhost:53150").AllowAnyMethod().AllowAnyHeader()
+            );
 #endif
 #if RELEASE
             app.UseCors(
 	            options => options.WithOrigins("https://www.hux-dev.com", "https://blog-cms-weld.vercel.app").AllowAnyMethod().AllowAnyHeader()
             );
 #endif
-            );
+
 
             app.UseDefaultFiles();
 
